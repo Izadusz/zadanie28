641b0474118e0e7b3908987e9f88d74b4fb7fffe
Iza Duszynska <iza.duszynska@wp.pl>
2020-09-02 18:24:33 +0200

    zadanie28

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let number1 = 8;
+let number2 = 10;
+
+if (number1 > number2) {
+console.log("Podana liczba jest większa od 10");
+} else {
+    console.log("Podana liczba nie jest większa od 10");
+}
